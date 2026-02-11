758c46d28f906f4ce9e6dd735fbe852c663800ea
Mrlamasat <mr.lamasat@gmail.com>
2026-02-11 23:28:32 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,14 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
+    import { defineConfig } from 'vite'
+import react from '@vitejs/react-refresh' // أو @vitejs/plugin-react حسب النسخة لديك
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: './', // يضاف هذا السطر هنا (عادة السطر 6 أو 7) لضمان عمل الروابط النسبية
+  plugins: [react()],
+})
     const env = loadEnv(mode, '.', '');
     return {
       server: {
